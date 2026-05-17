84e9ff406fe80acab637c23991e2259571b98c32
Yordy Guarnizo <yordyguarnizodev@gmail.com>
2026-05-17 18:46:34 -0500

    new exercises

--- main.js
+++ main.js
@@ -211,6 +211,7 @@ function fruitProcessor(apples, oranges) {
 }
 */
 
+/*
 function getTheElementValue(value) {
   return document.getElementById(value).value;
 }
@@ -258,3 +259,250 @@ function calculate() {
     document.getElementById("resultado").innerHTML = "No se puede dividir en 0";
   }
 }
+*/
+
+/*
+const calcAge = function (birthYear) {
+  return 2037 - birthYear;
+};
+
+const yearsUntilRetirement = function (birthYear, firstName) {
+  const age = calcAge(birthYear);
+  const retirement = 65 - age;
+  return retirement;
+  //return `${firstName} retires in ${retirement} years`;
+};
+
+console.log(yearsUntilRetirement(1990, "Yordy"));
+*/
+
+/*
+const calcAverage = (scoreOne, scoreTwo, scoreThree) =>
+  (scoreOne + scoreTwo + scoreThree) / 3;
+
+let scoreDolphin = calcAverage(85, 54, 41);
+let scoreKoalas = calcAverage(23, 34, 27);
+
+function checkWinner(avrDolphin, avrKoalas) {
+  if (avrDolphin >= avrKoalas * 2) {
+    console.log(`Dolphin ${avrDolphin} wins, Koalas ${avrKoalas}`);
+  } else if (avrKoalas >= avrDolphin * 2) {
+    console.log(`Koalas ${avrKoalas} wins, Dolphin ${avrDolphin}`);
+  } else {
+    console.log("No one wins");
+  }
+}
+
+checkWinner(scoreDolphin, scoreKoalas);
+*/
+
+/*
+const friends = ["Michael", "Steven", "Marcos"];
+console.log(friends);
+
+console.log(friends[0]); //Primer valor
+console.log(friends[2]); //ultimo valor
+console.log(friends.length); //Numero de elementos en el array
+console.log(friends[friends.length - 1]); //El -1 porque el array es 0 based, pero el length no
+
+friends[2] = "Otto"; //Se puede reemplazar partes de un array, pero no todo el array, por estar en una constante
+console.log(friends);
+
+const number = 80;
+const yordy = ["Yordy", "Guarnizo", 2037 - 1990, "teacher", friends, number]; //Podemos agregar todo tipo de expresiones
+console.log(yordy);
+
+//Exercise
+const calcAge = function (birthYear) {
+  return 2037 - birthYear;
+};
+
+const years = [1990, 1993, 1999, 2000, 2025];
+
+const age1 = calcAge(years[0]);
+const age3 = calcAge(years[years.length - 1]); //Para calcular los valores, necesitamos decirle cual en el array vamos a usar
+console.log(age1, age3);
+
+const ages = [
+  calcAge(years[0]),
+  calcAge(years[1]),
+  calcAge(years[years.length - 1]),
+];
+console.log(ages);
+//Podemos crear otro array, con los resultados de las funciones
+*/
+
+//Arrays
+/*
+const friends = ["Michael", "Steven", "Marcos"];
+
+//Add elements
+const newLength = friends.push("Jay"); //Agregar un valor, lo hara al final
+// si lo agregamos a una constante nos muestra es el numero de elementos en el array
+console.log(newLength);
+
+friends.unshift("John"); //Agrega un valor, al comienzo del array
+console.log(friends);
+
+//Remove elements
+friends.pop(); //Delete elements, there is no need to add any argument
+//If we add that in a variable, it will show the element that was deleted
+
+console.log(friends);
+
+friends.shift(); //Delete the first item
+
+//Find elements
+friends.unshift("Santos");
+console.log(friends);
+console.log(friends.indexOf("Steven")); //Nos dice en que parte del array esta
+
+console.log(friends.includes("Steven")); //Busca si esta en el array, en este caso esta, por lo tanto es true
+console.log(friends.includes("Bob")); //No hay ningun Bob, arroja false
+//Funciona con strict, quiere decir que si uno pone un number 23, y busca '23', arrojara false
+*/
+
+/*
+const calTip = function (bill) {
+  if (bill >= 50 && bill <= 300) {
+    return bill * 0.15;
+  } else {
+    return bill * 0.2;
+  }
+};
+
+const bills = [125, 555, 44];
+const tips = [
+  calTip(bills[0]),
+  calTip(bills[1]),
+  calTip(bills[bills.length - 1]),
+];
+const totals = [
+  bills[0] + tips[0],
+  bills[1] + tips[1],
+  bills[bills.length - 1] + tips[bills.length - 1],
+];
+
+console.log(tips, totals);
+*/
+
+/*
+const jonas = {
+  firstName: "Yordy",
+  lastName: "Guarnizo",
+  age: "26",
+  job: "Call Center Agent",
+  friends: ["Carlos", "Javier", "Hernando"],
+};
+
+console.log(jonas);
+console.log(jonas.lastName); //Traer el key lastName e imprimir el value
+console.log(jonas["lastName"]); //Lo mismo, pero en este se pueden poner expresiones
+
+const nameKey = "Name";
+
+console.log(jonas["first" + nameKey]); //first + name Key = firstName
+console.log(jonas["last" + nameKey]); // Same as above
+*/
+
+//const interestedIn = prompt(
+//  "What do you want to know about Jonas? Choose between firstName, lastName, job and friends",
+//);
+
+// If and else where the machine is going to see if something exist or not
+/*
+console.log(jonas[interestedIn]);
+
+if (jonas[interestedIn]) {
+  console.log(jonas[interestedIn]);
+} else {
+  console.log("Wrong request");
+}
+*/
+
+// Add elements into the object
+
+/*
+jonas.location = "Colombia"; //Agregar esto al objeto
+jonas["twitter"] = "@dev.com"; //Agregarlo al objeto pero con otra forma
+
+console.log(jonas);
+
+console.log(
+  `${jonas.firstName} has ${jonas.friends.length} friends, and his best friend is called ${jonas.friends[0]}`,
+);
+*/
+
+const jonas = {
+  firstName: "Yordy",
+  lastName: "Guarnizo",
+  birthYear: "1999",
+  job: "Call center agent",
+  friends: ["Carlos", "Javier", "Miler"],
+  hasDrivenLicense: true,
+
+  /*
+  calcAge: function (birthYear) {
+    return 2037 - birthYear;
+  }, */
+
+  // El objeto tiene el this, es decir podemos poner lo que esta en el objeto
+  // Al poner this.bithYear, no necesitamos un parametro, vamos directamente al jonas.bithYear
+
+  /*
+  calcAge: function () {
+    return 2037 - this.birthYear;
+  },
+  */
+
+  calcAge: function () {
+    this.age = 2037 - this.birthYear; //Creamos otra key para el objeto
+    return this.age; //Lo devolvemos
+  },
+
+  getSummary: function () {
+    console.log(
+      `Yordy is a ${this.age} years old ${this.job}, and he has ${this.hasDrivenLicense ? "a drivers license" : "no drivers license"}`,
+    );
+  },
+};
+
+//console.log(jonas.calcAge()); //llamar el metodo
+//console.log(jonas["calcAge"](1999)); // llamar el metodo pero con el otro formato
+
+console.log(jonas.calcAge()); //Ejecutamos el metodo
+console.log(jonas.age); //Ya se puede llamar en todo lado
+
+jonas.getSummary();
+
+const mark = {
+  fullName: "Mark Miller",
+  mass: 78,
+  heigth: 1.69,
+
+  calcBMI: function () {
+    this.bmi = this.mass / (this.heigth * this.heigth);
+    return this.bmi;
+  },
+};
+
+const john = {
+  fullName: "John Smith",
+  mass: 92,
+  heigth: 1.95,
+
+  calcBMI: function () {
+    this.bmi = this.mass / (this.heigth * this.heigth);
+    return this.bmi;
+  },
+};
+
+mark.calcBMI();
+john.calcBMI();
+
+console.log(mark.bmi);
+console.log(john.bmi);
+
+console.log(
+  `${mark.bmi > john.bmi ? `${mark.fullName} BMI (${mark.bmi}) is higher than ${john.fullName} (${john.bmi})` : `${john.fullName} BMI (${john.bmi}) is higher than ${mark.fullName} (${mark.bmi})`}`,
+);
